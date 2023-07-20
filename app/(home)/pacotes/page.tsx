ae6b820c7051a68528245b78ad6239284a201375
Abraão Albuquerque <abraao.hs2@gmail.com>
2023-07-20 17:45:09 -0300

    :whench: mudanças minimas em arquivos

--- app/(home)/pacotes/page.tsx
+++ app/(home)/pacotes/page.tsx
@@ -1,6 +1,9 @@
+import Badge from "@/components/Badge";
+import Container from "@/components/Container";
 import PriceCard from "@/components/PriceCard";
 import Paragraph from "@/components/Text";
 import Title from "@/components/Title";
+import { MdDone } from "react-icons/md";
 
 export const metadata = {
   title: "Pacotes",
@@ -11,19 +14,90 @@ export const metadata = {
 export default function PacotesPage() {
   return (
     <>
-      <Title>
-        PACOTES DE PARTICIPAÇÃO
-      </Title>
-      <Paragraph>
-        Descubra os diferentes níveis de participação oferecidos no evento SEELECT e escolha 
-        o que melhor se adequa às suas necessidades e orçamento. Explore os pacotes Básico, 
-        Premium e VIP, cada um com benefícios exclusivos para uma experiência personalizada 
-        e enriquecedora. Escolha o seu nível de participação e aproveite ao máximo o SEELECT!
-      </Paragraph>
+      <Container className="w-full pt-32 bg-white">
+        <Title className="text-2xl text-black text-center">
+          ESCOLHA UM PACOTE
+        </Title>
+        <Paragraph className="text-black/40 text-sm text-center">
+          Veja a melhor dentre as 3 opções abaixo:
+        </Paragraph>
 
-      <PriceCard price={30} title="Pacote Básico" description="teste"/>
-      <PriceCard price={60} title="Pacote Premium" description="teste"/>
-      <PriceCard price={120} title="Pacote VIP" description="teste"/>
+        <div>
+          <PriceCard price={30} title="Pacote Básico" stars={1}>
+            <ul className="text-black font-thin italic mt-4 mb-8">
+              <li className="flex gap-1 mt-2 text-sm">
+                <MdDone size={18} fill={"green"} />
+                Lorem ipsum dolor sit amet .
+              </li>
+              <li className="flex gap-1 mt-2 text-sm">
+                <MdDone size={18} fill={"green"} />
+                Lorem ipsum dolor sit amet.
+              </li>
+              <li className="flex gap-1 mt-2 text-sm">
+                <MdDone size={18} fill={"green"} />
+                Lorem ipsum dolor sit amet.
+              </li>
+              <li className="flex gap-1 mt-2 text-sm">
+                <MdDone size={18} fill={"green"} />
+                Lorem ipsum dolor sit amet.
+              </li>
+            </ul>
+          </PriceCard>
+
+          <Badge
+            value="POPULAR"
+            className="py-2 text-sm top-0 translate-y-2/3 -translate-x-1/2 text-cian-700 border border-cian-700"
+          >
+            <PriceCard
+              price={60}
+              destack={true}
+              title="Pacote Premium"
+              stars={2}
+            >
+              <ul className="font-thin italic mt-4 mb-8">
+                <li className="flex gap-1 mt-2 text-sm">
+                  <MdDone size={18} fill={"#84d1cf"} className="min-w-[20px]" />
+                  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit.
+                </li>
+                <li className="flex gap-1 mt-2 text-sm">
+                  <MdDone size={18} fill={"#84d1cf"} className="min-w-[20px]" />
+                  Lorem ipsum dolor sit amet.
+                </li>
+                <li className="flex gap-1 mt-2 text-sm">
+                  <MdDone size={18} fill={"#84d1cf"} className="min-w-[20px]" />
+                  Lorem ipsum dolor sit amet.
+                </li>
+                <li className="flex gap-1 mt-2 text-sm">
+                  <MdDone size={18} fill={"#84d1cf"} className="min-w-[20px]" />
+                  Lorem ipsum dolor sit amet.
+                </li>
+              </ul>
+            </PriceCard>
+          </Badge>
+
+          <PriceCard price={120} title="Pacote VIP" stars={3}>
+            <ul className="text-black font-thin italic mt-4 mb-8">
+              <li className="flex gap-1 mt-2 text-sm">
+                <MdDone size={18} fill={"green"} />
+                Lorem ipsum dolor sit amet .
+              </li>
+              <li className="flex gap-1 mt-2 text-sm">
+                <MdDone size={18} fill={"green"} />
+                Lorem ipsum dolor sit amet.
+              </li>
+              <li className="flex gap-1 mt-2 text-sm">
+                <MdDone size={18} fill={"green"} />
+                Lorem ipsum dolor sit amet.
+              </li>
+              <li className="flex gap-1 mt-2 text-sm">
+                <MdDone size={18} fill={"green"} />
+                Lorem ipsum dolor sit amet.
+              </li>
+            </ul>
+          </PriceCard>
+        </div>
+      </Container>
     </>
   );
 }
---
+++ components/Badge/index.tsx
@@ -0,0 +1,33 @@
+import { FunctionComponent, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface BadgeProps {
+  children: ReactNode;
+  className?: string;
+  value?: string | number;
+}
+
+const Badge: FunctionComponent<BadgeProps> = ({
+  children,
+  className,
+  value,
+}) => {
+  return (
+    <div className="relative">
+      {children}
+      {value && (
+        <div
+          style={{ writingMode: "vertical-lr", textOrientation: "upright" }}
+          className={twMerge(
+            " absolute rounded-full border flex items-center justify-center text-dark bg-white font-bold z-50",
+            className
+          )}
+        >
+          {value}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Badge;
