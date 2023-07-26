a5dfaefc68caf1d21dfc29c111b63009f562b034
Abraão Albuquerque <abraao.hs2@gmail.com>
2023-07-26 16:07:22 -0300

    :lipstick: feat: adicionando decoracoes na home

--- components/SECTIONS/PresentationSection/index.tsx
+++ components/SECTIONS/PresentationSection/index.tsx
@@ -1,21 +1,27 @@
 "use client";
 import Image from "next/image";
-import seelect_logo from "@/public/icone_seelect.webp";
 import Text from "@/components/Text";
 import FloatButton from "@/components/FloatButton";
 import Container from "@/components/Container";
 
+import seelect_logo from "@/public/icone_seelect.webp";
+import squares_deco from "@/public/SVG/squares-deco.svg";
+import elipse from "@/public/SVG/elipse-deco.svg";
+
 export default function PresentationSection() {
   return (
     <section>
-      <Container className="flex flex-col-reverse lg:flex-row lg:items-center">
-        <div>
-          <h1
-            className={`text-2xl mb-3 lg:mb-6 font-bold tracking-wide lg:text-4xl xl:text-5xl`}
-          >
-            TERCEIRA SEMANA DAS ENGENHARIAS DE COMPUTAÇÃO, ELÉTRICA E
-            TELECOMUNICAÇÕES.
-          </h1>
+      <Container className="flex relative z-10 flex-col-reverse lg:flex-row lg:items-center">
+        <div className="">
+          <div className="backdrop-blur-md p-2 rounded-md">
+            <h1
+              className={`text-2xl mb-3 lg:mb-6 font-bold tracking-wide lg:text-4xl xl:text-5xl`}
+            >
+              TERCEIRA SEMANA DAS ENGENHARIAS DE COMPUTAÇÃO, ELÉTRICA E
+              TELECOMUNICAÇÕES.
+            </h1>
+          </div>
+
           <Text className="opacity-80 font-thin max-w-4xl">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus
             dicta veritatis debitis, illo provident voluptate, placeat soluta
@@ -34,6 +40,19 @@ export default function PresentationSection() {
             src={seelect_logo}
             priority={true}
             alt="logo da seelect"
+            className="lg:min-w-[360px]"
+          />
+        </figure>
+
+        <div className="m-auto w-1/2 absolute -z-10 top-0 left-0 -translate-y-2/3 -translate-x-1/2 mb-6 max-w-md">
+          <Image src={elipse} priority={true} alt="decoration" />
+        </div>
+
+        <figure className="m-auto w-1/2  absolute top-0 right-0 -translate-y-1/2 translate-x-1/2 mb-6 max-w-xs">
+          <Image
+            src={squares_deco}
+            priority={true}
+            alt="decoration"
             className="lg:min-w-[360px]"
           />
         </figure>
