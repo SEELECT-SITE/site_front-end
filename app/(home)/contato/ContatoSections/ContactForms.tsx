65e262633ee9e82ae7cdd1b4cb7fb742787c9b09
Abraão Albuquerque <abraao.hs2@gmail.com>
2023-07-26 21:05:16 -0300

    :construction: desenvolvendo pagina de contato

---
+++ app/(home)/contato/ContatoSections/ContactForms.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const createContactSchema = z.object({
+  email: z
+    .string()
+    .nonempty("O email é obrigatorio")
+    .email("Formato de e-mail invalido"),
+  nome: z
+    .string()
+    .nonempty("O nome é obrigatorio")
+    .transform((name) => {
+      return name
+        .trim()
+        .split(" ")
+        .map((word) => {
+          return word[0].toLocaleUpperCase().concat(word.substring(1));
+        })
+        .join(" ");
+    }),
+  telefone: z.coerce
+    .number({ invalid_type_error: "Digite um numero valido" })
+    .gte(10000000, "Digite um numero valido")
+    .lte(9999999999999, "Digite um numero valido"),
+});
+
+type CreateContactData = z.infer<typeof createContactSchema>;
+
+import FloatButton from "@/components/FloatButton";
+import Input from "@/components/Input";
+import TextAreaInput from "@/components/TextAreaInput";
+import Decoration from "@/components/SECTIONS/Cronograma/DecorationStripes/decoration";
+
+export default function ContactForms() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateContactData>({
+    resolver: zodResolver(createContactSchema),
+  });
+
+  function createContact(data: any) {
+    console.log(data);
+  }
+  return (
+    <form
+      onSubmit={handleSubmit(createContact)}
+      className="w-full flex flex-col gap-8 border-dark bg-dark border py-8 px-4 rounded-2xl max-w-md"
+    >
+      <Decoration shadowClassname=" w-full h-8" type="light" />
+      <Input
+        required
+        placeholder="Nome"
+        type="text"
+        errorMsg={errors.nome?.message as string}
+        register={register("nome")}
+        className=" border-cian-400"
+      />
+      <Input
+        required
+        placeholder="E-mail"
+        errorMsg={errors.email?.message as string}
+        type="email"
+        register={register("email")}
+        className=" border-cian-400"
+      />
+      <Input
+        required
+        placeholder="Telefone"
+        errorMsg={errors.telefone?.message as string}
+        type="text"
+        register={register("telefone")}
+        className=" border-cian-400"
+      />
+      <TextAreaInput label="Mensagem" rows={4} className=" border-cian-400" />
+      <FloatButton type="submit" className=" text-xl" shadowClassname="w-full">
+        Enviar
+      </FloatButton>
+    </form>
+  );
+}
