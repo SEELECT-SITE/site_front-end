f23df21e0801ce67f0b44e76895a7057a9c0f037
Abraão Albuquerque <abraao.hs2@gmail.com>
2023-07-27 10:50:29 -0300

    centralizando LoginForms

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -19,7 +19,8 @@ export default function Login() {
     <>
       <div className="w-full h-screen lg:flex lg:flex-row-reverse">
         <BannerLogin />
-        <div className="w-full mt-6 h-full lg:mt-0 bg-white lg:w-1/2  lg:border-r-4 border-black overflow-hidden m-auto">
+        <div className="w-full mt-6 h-full lg:mt-0 bg-white lg:w-1/2  lg:border-r-4 border-black overflow-hidden m-auto flex flex-col justify-start">
+          
           <Container className="w-full m-auto max-w-lg 2xl:p-8">
             <FormsLogin />
             <div className="flex flex-wrap w-full max-w-sm m-auto justify-between mt-2 ">
