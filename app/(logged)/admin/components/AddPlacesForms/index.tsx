5116f0b043ff0677ada7389327783a27082ec78b
abrxao <abrxao@gmail.com>
2023-10-16 09:24:27 -0300

    teste pet

--- app/(logged)/admin/components/AddPlacesForms/index.tsx
+++ app/(logged)/admin/components/AddPlacesForms/index.tsx
@@ -14,28 +14,14 @@ import axios from "axios";
 import Title from "@/components/Title";
 import removeElem from "@/utils/removeElem";
 import { DJANGO_URL } from "@/utils/consts";
+import { IoAddOutline } from "react-icons/io5";
 
 const createAddPlaceFormsSchema = z.object({
-  location: z.string(),
-  url_location: z.string(),
-  capacity: z.string(),
+  location: z.string().nonempty("Preencha o campo"),
+  url_location: z.string().nonempty("Preencha o campo"),
+  capacity: z.number().min(1),
   equipaments: z.string(),
 });
-
-function SelectEquipaments() {
-  return (
-    <div>
-      <ul className="flex gap-2">
-        <li className="bg-slate-500 gap-1 p-2 rounded-md items-center flex justify-between">
-          23 cadeiras{" "}
-          <div className="p-1 rounded-3xl bg-slate-900 hover:bg-black">
-            <MdClose size={16} />
-          </div>
-        </li>
-      </ul>
-    </div>
-  );
-}
 
 type CreateAddPlaces = z.infer<typeof createAddPlaceFormsSchema>;
 
@@ -43,13 +29,17 @@ export default function AddPlaceForms({ Token }: { Token: string }) {
   const {
     register,
     handleSubmit,
+    setError,
+    getValues,
+    reset,
+    setValue,
     formState: { errors },
   } = useForm<CreateAddPlaces>({
     resolver: zodResolver(createAddPlaceFormsSchema),
   });
   const [errorReq, setErrorReq] = useState<any>("");
   const errorsDiv = useRef<HTMLDivElement | null>(null);
-  const [equipaments, setEquipaments] = useState<string[]>([""]);
+  const [equipaments, setEquipaments] = useState<string[]>([]);
 
   function toogleElements(id: string) {
     if (equipaments.includes(id)) {
@@ -61,13 +51,17 @@ export default function AddPlaceForms({ Token }: { Token: string }) {
   }
 
   async function addPlace(data: CreateAddPlaces) {
-    const { url_location, location, capacity, equipaments } = data;
+    const { url_location, location, capacity } = data;
     setErrorReq("");
     const formData = new URLSearchParams();
     formData.append("url_location", url_location);
     formData.append("location", location);
-    formData.append("equipaments", equipaments);
-    formData.append("capacity", capacity);
+    equipaments.forEach((elem) => {
+      if (elem) {
+        formData.append("equipaments", elem);
+      }
+    });
+    formData.append("capacity", capacity.toString());
 
     const headers = {
       "Content-Type": "application/x-www-form-urlencoded",
@@ -83,6 +77,8 @@ export default function AddPlaceForms({ Token }: { Token: string }) {
       );
     } catch (error) {
       console.log(error);
+    } finally {
+      reset();
     }
   }
   return (
@@ -109,17 +105,63 @@ export default function AddPlaceForms({ Token }: { Token: string }) {
           <Input
             placeholder="Capacidade do local"
             errorMsg={errors.capacity?.message as string}
-            type="text"
-            register={register("capacity")}
+            type="number"
+            register={register("capacity", { valueAsNumber: true })}
           />
-          <Input
-            placeholder="Equipamentos"
-            errorMsg={errors.equipaments?.message as string}
-            type="text"
-            register={register("equipaments")}
-          />
+
+          <div className="relative rounded-lg border-2 bg-black border-black ">
+            <Input
+              placeholder="Equipamentos (Ex:20 cadeiras)"
+              errorMsg={errors.equipaments?.message as string}
+              required={false}
+              register={register("equipaments")}
+              type="text"
+              className="my-0"
+            />
+            <button
+              type="button"
+              className="absolute z-10 right-0 top-1/2 -translate-y-1/2 bg-white h-full p-3 flex items-center justify-center rounded-tr-lg rounded-br-lg border-l text-dark"
+              onClick={(e) => {
+                var currentEquipament = getValues("equipaments");
+                console.log(currentEquipament);
+                if (currentEquipament) {
+                  var aux = equipaments;
+                  aux.push(currentEquipament);
+                  setEquipaments(aux);
+                  setValue("equipaments", "");
+                } else {
+                  setError("equipaments", {
+                    type: "manual",
+                    message: "Não pode estar vazio",
+                  });
+                }
+              }}
+            >
+              <IoAddOutline size={24} />
+            </button>
+          </div>
           <div>
-            <SelectEquipaments />
+            <ul className="flex gap-2">
+              {equipaments.map((elem, index) => {
+                return (
+                  <li
+                    key={elem + index}
+                    className="bg-slate-500 gap-1 p-2 rounded-md items-center flex justify-between"
+                  >
+                    {elem}
+                    <button
+                      type="button"
+                      className="p-1 rounded-3xl bg-slate-900 hover:bg-black"
+                      onClick={(e) => {
+                        setEquipaments(removeElem(equipaments, elem));
+                      }}
+                    >
+                      <MdClose className="pointer-events-none" size={16} />
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
           {errorReq !== "" && (
             <div
