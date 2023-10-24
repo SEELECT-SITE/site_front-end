867cd64e2f45610dd9cac41f4b728c980c7fcdc1
abrxao <abrxao@gmail.com>
2023-10-24 15:47:31 -0300

    rerun ngrok server

--- utils/consts.ts
+++ utils/consts.ts
@@ -1,4 +1,4 @@
-export const DJANGO_URL = "https://70ac-200-19-186-128.ngrok-free.app/";
+export const DJANGO_URL = "https://e6be-200-19-186-128.ngrok-free.app/";
 
 //https:70ac-200-19-186-128.ngrok-free.app
 export const IES_CEARA = [
